289c7bafd7e4c9bc268ef47071aa86c142194c88
LambdaLight4205 <lambda.light@proton.me>
2025-01-09 17:39:02 +0100

    Delete file

--- test.py
+++
@@ -1,6 +0,0 @@
-a = ""
-var1 = (a, a, a, a, a, a, a, a)
-# aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaff
-# a
-#  ,, , ( ( ) ) ) : :::  d :a
-var2 = str( a + " () " )
